5688fa7d7f70fe1b2661bb1309121f124b89854f
ParcheEnGit <adrianpereztapia@gmail.com>
2026-06-23 01:15:21 -0400

    feat(tests): refactorizar prueba TC-09 para utilizar función de inicio de sesión y simplificar el flujo de exportación de CV

--- tests/TC09_exportar_cv.spec.js
+++ tests/TC09_exportar_cv.spec.js
@@ -1,17 +1,13 @@
 import { test, expect } from '@playwright/test';
+import { login } from './helpers.js';
 
-test('test', async ({ page }) => {
-  await page.goto('http://kawisoft.tis.cs.umss.edu.bo/');
-  await page.getByRole('banner').getByRole('link', { name: 'Iniciar sesión' }).click();
-  await page.getByRole('textbox', { name: 'Correo institucional' }).click();
-  await page.getByRole('textbox', { name: 'Correo institucional' }).fill('cttandrea34@gmail.com');
-  await page.getByRole('textbox', { name: 'Contraseña' }).click();
-  await page.getByRole('textbox', { name: 'Contraseña' }).fill('andy2000');
-  await page.getByRole('textbox', { name: 'Contraseña' }).press('Enter');
-  await page.getByRole('button', { name: 'Iniciar sesión → en Dev' }).click();
+test('TC-09 Exportar CV', async ({ page }) => {
+  await login(page);
   await page.getByRole('button', { name: 'Exportar CV' }).click();
   await page.getByRole('button', { name: 'PDF A4 / print-ready' }).click();
   const downloadPromise = page.waitForEvent('download');
   await page.getByRole('button', { name: 'Descargar reporte' }).click();
   const download = await downloadPromise;
+  expect(download.suggestedFilename()).toMatch(/\.(pdf|html)$/i);
 });
+
