5f095588a38dbbe383ed547c019326ff0452a58c
ParcheEnGit <adrianpereztapia@gmail.com>
2026-06-23 01:14:13 -0400

    feat(tests): refactorizar la prueba de registro de usuario para utilizar funciones helper de datos

--- tests/registro.spec.js
+++ tests/registro.spec.js
@@ -1,22 +1,24 @@
 import { test, expect } from '@playwright/test';
+import { testUser } from './helpers.js';
 
 test('TC-01 Registro exitoso de usuario', async ({ page }) => {
-  await page.goto('http://kawisoft.tis.cs.umss.edu.bo/');
+  await page.goto('/');
 
   // Ir al formulario de registro
   await page.getByRole('banner').getByRole('button', { name: 'Registrarse' }).click();
 
   // Completar formulario
-  await page.getByRole('textbox', { name: 'Nombre completo' }).fill('Andrea Torrico');
-  await page.getByRole('textbox', { name: 'Correo institucional' }).fill('cttandrea34@gmail.com');
-  await page.getByRole('textbox', { name: 'Contraseña', exact: true }).fill('andy2000');
-  await page.getByRole('textbox', { name: 'Confirmar contraseña' }).fill('andy2000');
+  await page.getByRole('textbox', { name: 'Nombre completo' }).fill(testUser.name);
+  await page.getByRole('textbox', { name: 'Correo institucional' }).fill(testUser.email);
+  await page.getByRole('textbox', { name: 'Contraseña', exact: true }).fill(testUser.password);
+  await page.getByRole('textbox', { name: 'Confirmar contraseña' }).fill(testUser.password);
 
   // Registrar usuario
   await page.getByRole('button', { name: 'Crear mi perfil →' }).click();
 
   // Verificar que aparece el login
   await expect(
-    page.getByRole('button', { name: 'Iniciar sesión → en Dev' })
+    page.getByRole('button', { name: /Iniciar sesión/i })
   ).toBeVisible();
 });
+
