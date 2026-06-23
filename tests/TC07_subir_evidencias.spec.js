67abe1d526d748e389cdfc1ea6a61a9fb84b06d5
ParcheEnGit <adrianpereztapia@gmail.com>
2026-06-23 01:15:07 -0400

    feat(tests): refactorizar prueba TC-07 para utilizar función de inicio de sesión y mejorar la carga de archivos

--- tests/TC07_subir_evidencias.spec.js
+++ tests/TC07_subir_evidencias.spec.js
@@ -1,16 +1,12 @@
 import { test, expect } from '@playwright/test';
+import { setFirstFileInput, login } from './helpers.js';
 
-test('test', async ({ page }) => {
-  await page.goto('http://kawisoft.tis.cs.umss.edu.bo/');
-  await page.getByRole('banner').getByRole('link', { name: 'Iniciar sesión' }).click();
-  await page.getByRole('textbox', { name: 'Correo institucional' }).click();
-  await page.getByRole('textbox', { name: 'Correo institucional' }).fill('cttandrea34@gmail.com');
-  await page.getByRole('textbox', { name: 'Contraseña' }).click();
-  await page.getByRole('textbox', { name: 'Contraseña' }).fill('andy2000');
-  await page.getByRole('button', { name: 'Iniciar sesión → en Dev' }).click();
+test('TC-07 Subir evidencias', async ({ page }) => {
+  await login(page);
   await page.getByRole('button', { name: 'Proyectos' }).click();
   await page.getByRole('button', { name: 'Evidencias' }).click();
   await page.getByRole('button', { name: 'Choose File' }).click();
-  await page.getByRole('button', { name: 'Choose File' }).setInputFiles('Captura de pantalla 2026-06-06 153303.png');
+  await setFirstFileInput(page, 'sample.png');
   await page.getByRole('button', { name: 'Confirmar Envío' }).click();
 });
+
